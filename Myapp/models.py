337fdba78e7b087d0fb6381d8cbe7362dc76ef57
parveez <parveez1991@gmail.com>
2022-09-11 14:08:11 +0530

    second commit

--- Myapp/models.py
+++ Myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    fname = models.CharField(max_length=20)
+    empid = models.IntegerField(max_length=10)
+    role = models.CharField(max_length=20)
+    add = models.TextField()
+    
